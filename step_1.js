d4ea40a1746f9ad07ffbc80a9b2faec237bffb07
zaliesov <Zalesov_@ukr.net>
2019-09-25 19:00:59 +0300

    home1

---
+++ step_1.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+console.error('error');
+console.info('info');
+//alert('hello');
+var one = 1;
+console.log(one);
+var two = 2;
+console.log(two);
---
+++ step_2.js
@@ -0,0 +1,12 @@
+var obj = {
+	age: 25,
+	name: "Sasha",
+	isMarried: false,
+	email: null,
+	skype: undefined
+}
+console.log(obj);
+document.write();
+var years = prompt('how old are you?',100);
+
+alert(years);
